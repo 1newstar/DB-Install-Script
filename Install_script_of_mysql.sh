2302ca32a673638bf60b73a3b4af1db1b2475b25
mag <mag@github.com>
2018-03-29 21:18:02 +0800

    20180328

--- Install_script_of_mysql.sh
+++ Install_script_of_mysql.sh
@@ -1,17 +1,17 @@
 
 
-
 function init_env()
 {
 	yum -y install libaio
 	groupadd mysql
 	useradd -s /bin/false -r -g mysql  mysql
 	cd /usr/local/src
-	tar -xzf mysql-5.7.20-linux-glibc2.12-x86_64.tar.gz -C ../
+    wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-$DB_VERSION-linux-glibc2.12-x86_64.tar.gz
+	tar -xzf mysql-$DB_VERSION-linux-glibc2.12-x86_64.tar.gz -C ../
 	cd /usr/local/
-	mv  mysql-5.7.20-linux-glibc2.12-x86_64  mysql-5.7.20
-	chown -R mysql.mysql mysql-5.7.20
-	ln -s /usr/local/mysql-5.7.20 /usr/local/mysql
+#	mv  mysql-5.7.20-linux-glibc2.12-x86_64  mysql-5.7.20
+	chown -R mysql.mysql /usr/local/mysql-$DB_VERSION-linux-glibc2.12-x86_64 
+	ln -s /usr/local/mysql-$DB_VERSION-linux-glibc2.12-x86_64 /usr/local/mysql
         mkdir /data/tmp
         chown -R mysql.mysql /data/tmp
         chmod 750 /data/tmp
@@ -135,68 +135,68 @@ log_timestamps = SYSTEM
 basedir=$BASEDIR
 datadir=$DATADIR
 open_files_limit = 3072
-back_log = 100                          
-max_connections = 500                              
+back_log = 100
+max_connections = 500
 max_connect_errors = 100000
 table_open_cache = 1024
 external-locking = false
-max_allowed_packet = 32M                     
+max_allowed_packet = 32M
 sort_buffer_size = 1M
 join_buffer_size = 1M
 thread_cache_size = 51
-#query_cache_size = 0                                    
+#query_cache_size = 0
 tmp_table_size = 96M
 max_heap_table_size = 96M
-slow_query_log = 1                                        
+slow_query_log = 1
 slow_query_log_file = $DATADIR/slow.log
 log_error = $DATADIR/mysql.err
-long_query_time = 0.5                                     
+long_query_time = 0.5
 server-id = $sid
 
-pid_file = $DATADIR/mysql.pid                                         
+pid_file = $DATADIR/mysql.pid
 log_bin = $DATADIR/mysql-bin
-sync_binlog = 1                                          
+sync_binlog = 1
 relay_log = $DATADIR/mysql-relay-bin
 binlog_cache_size = 4M
 max_binlog_cache_size = 128M
 max_binlog_size = 1024M
-expire_logs_days = 7               
+expire_logs_days = 7
 key_buffer_size = 32M
 read_buffer_size = 1M
 read_rnd_buffer_size = 16M
 bulk_insert_buffer_size = 64M
 character-set-server=utf8mb4
 default-storage-engine = InnoDB
-binlog_format=row                                         
+binlog_format = row
 
-symbolic-links = 0   
-tmpdir=/data/tmp
-secure_file_priv=/data/tmp
+symbolic-links = 0
+tmpdir = /data/tmp
+secure_file_priv = /data/tmp
 
-relay_log_recovery=1                           
-replicate_wild_ignore_table=mysql.%                       
-replicate_wild_ignore_table=information_schema.%                 
+relay_log_recovery=1
+replicate_wild_ignore_table=mysql.%
+replicate_wild_ignore_table=information_schema.%
 replicate_wild_ignore_table=test.%
 replicate_wild_ignore_table=sys.%
-                       
+
 gtid_mode=on
 log_slave_updates=1
 enforce_gtid_consistency=1
 sql_mode = NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION
 
 relay_log_info_repository = table
-master_info_repository = table                       
+master_info_repository = table
 
-transaction_isolation = REPEATABLE-READ                                    
-innodb_buffer_pool_size = $innodb_buffer_pool_size               
+transaction_isolation = REPEATABLE-READ
+innodb_buffer_pool_size = $innodb_buffer_pool_size
 innodb_data_home_dir= $DATADIR
-innodb_data_file_path = ibdata1:1024M:autoextend        
-innodb_flush_log_at_trx_commit = 0                        
-innodb_log_buffer_size = 16M                                  
-innodb_log_file_size = $innodb_log_file_size                       
-innodb_log_files_in_group = 2                            
-innodb_max_dirty_pages_pct = 50                      
-innodb_file_per_table = 1                         
+innodb_data_file_path = ibdata1:1024M:autoextend
+innodb_flush_log_at_trx_commit = 0
+innodb_log_buffer_size = 16M
+innodb_log_file_size = $innodb_log_file_size
+innodb_log_files_in_group = 2
+innodb_max_dirty_pages_pct = 50
+innodb_file_per_table = 1
 #innodb_file_format = Barracuda
 innodb_locks_unsafe_for_binlog = 0
 
@@ -238,7 +238,8 @@ MAX_SID=`expr $MIN_SID + $N - 1 `
 BASEDIR=/usr/local/mysql
 TMPDIR=/data/tmp
 innodb_buffer_pool_size=4096M
-innodb_log_file_size=2048M
+innodb_log_file_size=1024M
+DB_VERSION=$1
 
 init_env
 
@@ -246,13 +247,14 @@ if [ $N -eq 1 ]
 	then
 		DATADIR=/data/mysql
 		mkdir -p $DATADIR
+        rm -rf $DATADIR/*
 		chown -R mysql.mysql $DATADIR
 		chmod 750 $DATADIR
                 SID=$MIN_SID
 		config_single
 		$BASEDIR/bin/mysqld --defaults-file=/etc/my.cnf --initialize --datadir=$DATADIR --basedir=$BASEDIR --user=mysql
-#		$BASEDIR/bin/mysqld_safe --defaults-file=/etc/my.cnf &
-	else 
+		$BASEDIR/bin/mysqld_safe --defaults-file=/etc/my.cnf &
+    else
 		port=3340
 		for sid in $(seq "$MIN_SID" "$MAX_SID")
 			do
@@ -260,17 +262,25 @@ if [ $N -eq 1 ]
 				DATADIR=/data/mysql/$port
                                 sock=/tmp/mysql"$port".sock
 				mkdir -p $DATADIR
+                rm -rf $DATADIR/*
 				chown -R mysql.mysql $DATADIR
 				chmod 750 $DATADIR
 				config_multi
 			done
 		config_mult_end
-		
+
                 port=3341
                 max_port=`expr $port + $N - 1`
 		for p in $(seq "$port" "$max_port")
 			do
-				$BASEDIR/bin/mysqld --defaults-file=/etc/my.cnf --initialize --datadir=/data/mysql/$p --basedir=$BASEDIR --innodb_data_file_path=ibdata1:1024M:autoextend --user=mysql
+				$BASEDIR/bin/mysqld --defaults-file=/etc/my.cnf --initialize --datadir=/data/mysql/$p --basedir=$BASEDIR --innodb_data_file_path=ibdata1:1024M:autoextend  --log_error=/data/mysql/$p/mysql.err --user=mysql
+                $BASEDIR/bin/mysqld_multi --defaults-file=/etc/my.cnf start $p
+                sleep 20s
+                PASS=`cat /data/mysql/$p/mysql.err |grep "A temporary password is generated for root@localhost:"|awk  '{print $11}'`
+                echo $PASS
+                read -s -p "PASS: " NEW_PASS
+                $BASEDIR/bin/mysql -uroot -p$PASS --connect-expired-password -e "alter user root@localhost identified by '$NEW_PASS';"
+
 			done
 fi
 
